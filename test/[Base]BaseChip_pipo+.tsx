ed1ecbef88235c61dc0849c6ddca0eb349db0ab0
der-den <denny1977@googlemail.com>
2021-01-17 21:01:27 +0100

    test map

---
+++ test/[Base]BaseChip_pipo+.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="[Base]BaseChip_pipo+" tilewidth="32" tileheight="32" tilecount="904" columns="8">
+ <image source="../[Base]BaseChip_pipo+.png" width="256" height="3616"/>
+</tileset>
